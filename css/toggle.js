114becf51540582699ab9e7fe1bb62e69dd201c5
phuchung97 <phuchung9794@gmail.com>
2021-09-23 22:21:06 +0900

    update resume

---
+++ css/toggle.js
@@ -0,0 +1,19 @@
+function toggleImage() {
+    $(".hiddenclickimg").toggle();
+}
+function toggleImage1() {
+    $(".hiddenclickimg1").toggle();
+}
+function toggleImage2() {
+    $(".hiddenclickimg2").toggle();
+}
+function toggleImage3() {
+    $(".hiddenclickimg3").toggle();
+}
+function toggleImage4() {
+    $(".hiddenclickimg4").toggle();
+}
+function toggleImage5() {
+    $(".hiddenclickimg5").toggle();
+}
+
